b4677f39b07b1a743dc904e951d71947f9f89f64
safar0101 <blacklover2920@email.com>
2023-03-29 15:58:29 +0530

    Skeleton

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-
 import './global.css';
-import Footer from './components/common/footer/Footer';
+import { BrowserRouter, Route,Routes} from 'react-router-dom';
 import Header from './components/common/header/Header';
+import Footer from './components/common/footer/Footer';
 import Home from './pages/home/Home';
+import Destinations from './pages/destinations/Destinations'
+import Places from './pages/places/Places'
+import Packages from './pages/packages/Packages1';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+
     <Header/>
-    <Home/>
+    <Routes>
+        <Route path="home" element={<Home />} />
+        <Route path="destinations" element={<Destinations />} />
+        <Route path="places" element={<Places/>} />
+        <Route path="packages" element={<Packages />} />
+        <Route path="About" element={<About/>} />
+        <Route path="contact" element={<Contact />} /> 
+    </Routes>
+
     <Footer/>
-    </>
+  </BrowserRouter>
   );
 }
 
---
+++ src/pages/destinations/Destinations.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Destinations = () => {
+  return (
+    <div>
+      <h1>Destinations Working !!</h1>
+    </div>
+  )
+}
+
+export default Destinations
