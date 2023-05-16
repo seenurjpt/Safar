91ed634ef2ddecbcf04f4dee60487ae864c89787
utsav173 <khatriutsav40@gmail.com>
2023-05-16 22:40:02 +0530

    add minor chnage

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
-    <React.StrictMode><App /></React.StrictMode>
-    
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
 );
 
 reportWebVitals();
