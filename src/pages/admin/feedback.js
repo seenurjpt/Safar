f94e395a4124fcbcd8665d510cfbdb87d723d7a3
safar0101 <blacklover2920@email.com>
2023-05-19 15:37:09 +0530

    888888

--- src/pages/admin/feedback.js
+++ src/pages/admin/feedback.js
@@ -20,9 +20,6 @@ const Feedback = () => {
   };
 
 
-  
-  
-
   useEffect(() => {
     getApi();
 
@@ -31,20 +28,10 @@ const Feedback = () => {
 
   return (
     <>
-    {/* <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"></link>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script> */}
+
       <div className="px-3 background-color">
         <div className="container-fluid">
           <div className="row g-3 py-5">
-            {/* <div className="col-md p-3">
-              <div className="p-3 bg-white shadow-sm  d-flex justify-content-around align-item-center rounded ">
-                <div>
-                  <h3 className="fs-2 pt-3">450</h3>
-                  <p className="fs-2">Visitor</p>
-                </div>
-                <i class="bi bi-people p-3 fs-1"></i>
-              </div>
-            </div> */}
 
 
             <div className="col-md p-3">
@@ -74,7 +61,7 @@ const Feedback = () => {
                     <tr>
                       <th scope="row">{index+1}</th>
                       {/* <td>{user.Star}   </td> */}
-                      {/* <td>{golu}</td> */}
+                      <td>{user.Star}</td>
                     
                       <td>{user.feedback}</td>
                       <td>
